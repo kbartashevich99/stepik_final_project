bfa4292435bbb71e6751a9cdf006215158117094
kbartashevich99 <kbartashevich99@gmail.com>
2021-04-29 19:56:54 +0300

    add locators for product page

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,6 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, "")
